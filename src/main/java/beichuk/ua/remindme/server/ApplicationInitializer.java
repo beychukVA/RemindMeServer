886d30fc0e5fbf4b2e2bc5430d3e8944eb8c7f9d
beychukVA <beychuk.v.a@gmail.com>
2016-07-29 14:34:49 +0300

    Создание скилета №1

---
+++ src/main/java/beichuk/ua/remindme/server/ApplicationInitializer.java
@@ -0,0 +1,27 @@
+package beichuk.ua.remindme.server;
+
+import beichuk.ua.remindme.server.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer
+{
+    private static final String DISPATCHER = "dspatcher";
+
+    public void onStartup(ServletContext servletContext) throws ServletException
+    {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(context));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
